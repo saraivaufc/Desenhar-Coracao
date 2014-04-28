4aeefa89e8eca68f20f391a3dde56a861115dbd7
Ciano <cianohanter@hotmail.com>
2014-04-27 21:21:54 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+
+int particionado;
+
+void desenha_coracao_cima(int n){
+	int meio=(n/2)/3;
+	int a=meio;
+	int b=meio*2;
+	int c=meio*2;
+	int d=meio*2;
+
+	for(int i=meio+1;i>=1;i--){
+		for(int k=a;k>=1;k--){
+			cout<<" ";
+		}
+		for(int l=b;l>=1;l--){
+			if(i==(meio+1) && (l==b || l==1 ))
+				cout<<"  ";
+			else
+				cout<<"# ";
+		}
+		for(int m=c;m>=1;m--){
+			cout<<" ";
+		}
+		for(int n=d;n>=1;n--){
+			if(i==(meio+1) && (n==d || n==1 ))
+				cout<<"  ";
+			else
+				cout<<"# ";
+			if(i==1)
+				particionado++;
+		}
+		cout<<endl;
+		a--;
+		b++;
+		c=c-2;
+		d++;
+	}
+}
+void desenha_coracao_meio(int n){
+	for(int i=1;i<(n/2)/3;i++){
+		for(int k=1;k<=particionado*2;k++){
+			cout<<"# ";
+		}
+		cout<<endl;
+	}
+}
+void desenha_coracao_baixo(int n){
+	int aux=particionado*2;
+	for(int i=1;i<=particionado*2;i+=2){
+		for(int m=1;m<i;m++){
+			cout<<" ";
+		}
+		for(int k=1;k<=aux;k++){
+			if((i==1 && k==1) || (i==1 && k==aux+1))
+				cout<<" ";
+			else
+				cout<<"# ";
+		}
+		cout<<endl;
+		aux-=2;
+	}
+}
+
+
+int main(){
+	cout<<"Digite o tamanho:";
+	int tam;
+	cin>>tam;
+	desenha_coracao_cima(tam);
+	desenha_coracao_meio(tam);
+	desenha_coracao_baixo(tam);
+	return 0;
+}
